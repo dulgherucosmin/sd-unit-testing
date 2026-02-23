5c2da9f8bc00cee0bb02214f3f8c4ef18f96ef5d
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-02-23 20:41:44 +0000

    add (failed) unit tests

--- griffith/ConversionsTest.java
+++ griffith/ConversionsTest.java
@@ -1,6 +1,34 @@
 // Dulgheru Cosmin Alexandru 3161052
 package griffith;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ConversionsTest {
+
+    Conversions converter = new Conversions();
+
+    @Test
+    public void euroToDollarTest() {
+        // positive tests
+        assertEquals(11.79, converter.euroToDollar(10), 1e-6);
+        assertEquals(23.58, converter.euroToDollar(20), 1e-6);
+        assertEquals(117.9, converter.euroToDollar(100), 1e-6);
+
+        // negative tests
+        assertEquals(-1, converter.euroToDollar(10), 1e-6);
+        assertEquals(-1, converter.euroToDollar(30), 1e-6);
+        assertEquals(-1, converter.euroToDollar(45), 1e-6);
+
+        // 0 tests
+        assertEquals(0, converter.euroToDollar(5), 1e-6);
+        assertEquals(0, converter.euroToDollar(25), 1e-6);
+        assertEquals(0, converter.euroToDollar(50), 1e-6);
+    }
+
+    @Test
+    public void euroToDollarNullTest() {
+        assertNull(converter.euroToDollar((Double) null));
+    }
 
 }

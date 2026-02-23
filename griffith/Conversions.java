eb91c7505aab59df5e3ab54f442478608f97e14c
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-02-23 20:01:28 +0000

    add stub methods

--- griffith/Conversions.java
+++ griffith/Conversions.java
@@ -3,4 +3,23 @@ package griffith;
 
 public class Conversions {
 
+    public double euroToDollar(double euro) {
+        return 0.0;
+    }
+
+    public double dollarToEuro(double dollar) {
+        return 0.0;
+    }
+
+    public int stringToInteger(String val) {
+        return 0;
+    }
+
+    public String integerToString(int val) {
+        return "";
+    }
+
+    public String switchCase(String val) {
+        return "";
+    }
 }

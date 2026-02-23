53dd54f82eb8b38d397603119f617f6dd8476198
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-02-23 20:55:40 +0000

    fix test values

--- griffith/ConversionsTest.java
+++ griffith/ConversionsTest.java
@@ -11,18 +11,18 @@ public class ConversionsTest {
     @Test
     public void euroToDollarTest() {
         // positive tests
-        assertEquals(11.79, converter.euroToDollar(10), 1e-6);
-        assertEquals(23.58, converter.euroToDollar(20), 1e-6);
-        assertEquals(117.9, converter.euroToDollar(100), 1e-6);
+        assertEquals(11.8, converter.euroToDollar(10), 1e-6);
+        assertEquals(23.6, converter.euroToDollar(20), 1e-6);
+        assertEquals(118.0, converter.euroToDollar(100), 1e-6);
 
         // negative tests
-        assertEquals(-1, converter.euroToDollar(10), 1e-6);
-        assertEquals(-1, converter.euroToDollar(30), 1e-6);
-        assertEquals(-1, converter.euroToDollar(45), 1e-6);
+        assertEquals(-1.0, converter.euroToDollar(-10), 1e-6);
+        assertEquals(-1.0, converter.euroToDollar(-30), 1e-6);
+        assertEquals(-1.0, converter.euroToDollar(-45), 1e-6);
 
         // 0 tests
-        assertEquals(0, converter.euroToDollar(5), 1e-6);
-        assertEquals(0, converter.euroToDollar(25), 1e-6);
-        assertEquals(0, converter.euroToDollar(50), 1e-6);
+        assertEquals(0, converter.euroToDollar(0), 1e-6);
+        assertEquals(0, converter.euroToDollar(0), 1e-6);
+        assertEquals(0, converter.euroToDollar(0), 1e-6);
     }
 }
